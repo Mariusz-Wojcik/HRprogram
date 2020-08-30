82da08c0c6b5e3fef222f14145556ec969b0c857
Mariusz Wójcik <36dragan@gmail.com>
2020-08-30 22:43:46 +0200

    serialize and deserialize XML

---
+++ HRprogram/FileHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HRprogram
+{
+    public class FileHelper<T> where T : new()
+    {
+        private string _filePath;
+        public FileHelper(string filepath)
+        {
+            _filePath = filepath;
+        }
+        public void SerializeToFile(T employees)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var streamWriter = new StreamWriter(_filePath))
+            {
+                serializer.Serialize(streamWriter, employees);
+                streamWriter.Close();
+            }
+        }
+
+        public T DeserializeFromFile()
+        {
+            if (!File.Exists(_filePath))
+                return new T();
+
+            var serializer = new XmlSerializer(typeof(T));
+            using (var streamReader = new StreamReader(_filePath))
+            {
+                var employees = (T)serializer.Deserialize(streamReader);
+                streamReader.Close();
+                return employees;
+            }
+        }
+    }
+}
